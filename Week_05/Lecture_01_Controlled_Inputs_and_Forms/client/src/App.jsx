5f2489bd01d3cdb1e68d514a1c087143d36b44b8
Cody <stevencthaller@gmail.com>
2023-08-24 09:58:16 -0600

    controlled inputs

--- Week_05/Lecture_01_Controlled_Inputs_and_Forms/client/src/App.jsx
+++ Week_05/Lecture_01_Controlled_Inputs_and_Forms/client/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import Forms from "./components/Forms";
 
 function App() {
+  console.time("App render");
   return (
     <>
       <Forms />
+      {console.timeEnd("App render")}
     </>
   );
 }
